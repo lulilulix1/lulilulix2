70d32b1fee39514fbf7edc206f5c332aa6636677
lulilulix1 <unknownalbanianperson@gmail.com>
2026-03-24 22:28:37 +0100

    Fix CORS: allow all for testing

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,8 @@ const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 
-// CORS i konfiguruar
-app.use(cors({
-  origin: ['https://orendion.com', 'https://www.orendion.com', 'http://localhost:3000'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'] // ← Shto Authorization
-}));
+// CORS i thjeshtë për testim – lejon çdo header dhe çdo origin
+app.use(cors());
 
 app.use(express.json());
 
